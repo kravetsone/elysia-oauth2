52bf97c70f5e79d139b21981cd6fbe1e3cbab8c9
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-07-21 20:28:00 +0300

    chore: name handlers for https://elysiajs.com/blog/elysia-11.html#opentelemetry

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ export function oauth2<Options extends ElysiaOauth2Options>(options: Options) {
 
 	return new Elysia({ name: "elysia-oauth2" })
 		.error("OAUTH2_REQUEST_ERROR", arctic.OAuth2RequestError)
-		.derive(({ cookie, query, redirect }) => {
+		.derive(function deriveOauth2Methods({ cookie, query, redirect }) {
 			return {
 				oauth2: {
 					createURL: async <Provider extends keyof Options>(
